94ca585fdb68a73b0d6c07b3ff43a8543a7abc45
Alec Leamas <leamas.alec@nowhere.net>
2022-05-26 19:13:18 +0200

    flatpak-fix

--- ci/circleci-build-flatpak.sh
+++ ci/circleci-build-flatpak.sh
@@ -33,21 +33,6 @@ exec > >(tee $builddir/build.log) 2>&1
 
 if [ -n "$TRAVIS_BUILD_DIR" ]; then cd $TRAVIS_BUILD_DIR; fi
 
-# Install extra build libs
-ME=$(echo ${0##*/} | sed 's/\.sh//g')
-EXTRA_LIBS=./ci/extras/extra_libs.txt
-if test -f "$EXTRA_LIBS"; then
-    while read line; do
-        sudo apt-get install $line
-    done < $EXTRA_LIBS
-fi
-EXTRA_LIBS=./ci/extras/${ME}_extra_libs.txt
-if test -f "$EXTRA_LIBS"; then
-    while read line; do
-        sudo apt-get install $line
-    done < $EXTRA_LIBS
-fi
-
 if [ -n "$CI" ]; then
     sudo apt update
 
